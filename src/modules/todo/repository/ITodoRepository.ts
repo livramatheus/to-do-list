bc7410421653cbbb5ce775da60f45bb9f74332a3
livramateus <livra.matheus@gmail.com>
2023-10-12 14:29:27 -0300

    feat: develop Delete notes endpoint (resolves #3)

--- src/modules/todo/repository/ITodoRepository.ts
+++ src/modules/todo/repository/ITodoRepository.ts
@@ -3,4 +3,5 @@ import ITodoItem from '../dto/IToDoItem'
 export default interface ITodoRepository {
   getAll(): Promise<ITodoItem[]>
   newItem(item: ITodoItem): Promise<ITodoItem>
+  delete(id: string): Promise<boolean>
 }
---
+++ src/modules/todo/useCase/deleteItem/DeleteItemUseCase.ts
@@ -0,0 +1,18 @@
+/* eslint no-useless-constructor: "off" */
+
+import { inject, injectable } from 'tsyringe'
+import ITodoRepository from '../../repository/ITodoRepository'
+
+@injectable()
+class DeleteItemUseCase {
+  constructor(
+    @inject('TodoRepository') private todoRepository: ITodoRepository,
+  ) {}
+
+  async execute(id: string): Promise<boolean> {
+    const result = await this.todoRepository.delete(id)
+    return result
+  }
+}
+
+export default DeleteItemUseCase
